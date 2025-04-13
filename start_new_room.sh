2ce57a3f502d8aac0dbbe35915aa43eb77d3f0f3
Blothhoondr <56238228+Blothhoondr@users.noreply.github.com>
2025-04-13 15:26:48 +0100

    Update start_new_room.sh

--- start_new_room.sh
+++ start_new_room.sh
@@ -116,6 +116,7 @@ echo -e "\n"
 exit 0
 fi
 
+sudo rm ~/MEGA/ports
 sudo apt install masscan -y
 sudo masscan -p1-65535 --max-rate=10000 $IP > $BOXDIR_GLOBAL/nmap/ports
 sudo ports=$(cat $BOXDIR_GLOBAL/nmap/ports | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')
